a0d83bc55ce377e22bc417d36a9aeb5e8000e965
Adrian <adrian@theround.com>
2025-10-27 18:10:52 +0000

    implement database setup

---
+++ database/mongoose.ts
@@ -0,0 +1,41 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+// Load environment variables
+dotenv.config();
+
+const MONGODB_URI = process.env.MONGO_URI;
+
+declare global {
+  var mongooseCache: {
+		conn: typeof mongoose | null;
+		promise: Promise<typeof mongoose> | null;
+	}
+}
+
+let cached = global.mongooseCache;
+
+if (!cached) {
+  cached = global.mongooseCache = { conn: null, promise: null };
+}
+
+export const connectToMongoDB = async () => {
+	if (!MONGODB_URI) throw new Error("MONGO_URI must be set in the environment variables");
+
+	if (cached.conn) return cached.conn;
+
+	if (!cached.promise) {
+		cached.promise = mongoose.connect(MONGODB_URI, {
+			bufferCommands: false,
+		});
+	}
+
+	try {
+		cached.conn = await cached.promise;
+	} catch (error) {
+		cached.promise = null;
+		throw error;
+	}
+
+	console.log(`Connected to MongoDB ${process.env.NODE_ENV} - ${MONGODB_URI}`);
+}
